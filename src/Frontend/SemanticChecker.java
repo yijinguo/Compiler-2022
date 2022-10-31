700f8f71dcb13c72af2757e0a4a9110cb4e57ff0
yijinguo <18962420740@189.cn>
2022-10-31 21:50:11 +0800

    try

--- src/Frontend/SemanticChecker.java
+++ src/Frontend/SemanticChecker.java
@@ -25,14 +25,21 @@ public class SemanticChecker implements ASTVisitor {
         NullType = GlobalScope.getType("null", null);
     }
 
-    public void visit(RootNode it){}
+    public void visit(RootNode it){
+        FuncDefNode mainFunc = GlobalScope.getFunc("main");
+        if (mainFunc == null || !mainFunc.returnType.type.typeName.equals("int") || mainFunc.params != null)
+            throw new semanticError("Without MainFunction", it.pos);
+        for (var def : it.DefList) {
+            def.accept(this);
+        }
+    }
 
     public void visit(ClassDefNode it){}
     public void visit(ClassBuildNode it){}
     public void visit(FuncDefNode it){
         if (currentScope != GlobalScope && !(currentScope instanceof classScope))
             throw new syntaxError("Can't Define Function Here", it.pos);
-        it.accept(this);
+        //it.accept(this);
         currentScope = new funcScope(currentScope, it.returnType, it.params);
         if (currentScope.parentScope instanceof classScope &&
                 it.funcName.equals(((classScope) currentScope.parentScope).ClassType.typeName))
